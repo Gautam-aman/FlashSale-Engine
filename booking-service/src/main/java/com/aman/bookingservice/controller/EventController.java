117eb625150ff5b18eb754edaccdcee4215fe511
Aman Gautam <amangautam2128@gmail.com>
2026-08-08 17:17:30 +0530

    created endpoint to create event

--- booking-service/src/main/java/com/aman/bookingservice/controller/EventController.java
+++ booking-service/src/main/java/com/aman/bookingservice/controller/EventController.java
@@ -1,4 +1,29 @@
 package com.aman.bookingservice.controller;
 
+import com.aman.bookingservice.dto.CreateEventRequest;
+import com.aman.bookingservice.entity.Event;
+import com.aman.bookingservice.service.EventService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/events")
+@RequiredArgsConstructor
 public class EventController {
+
+	private final EventService eventService;
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Event createEvent(@Valid @RequestBody CreateEventRequest request) {
+		return eventService.createEvent(request);
+	}
+
 }
