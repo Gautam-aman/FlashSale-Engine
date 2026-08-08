0659e67cc2179b6571501d37c205b6474ce655b7
Aman Gautam <amangautam2128@gmail.com>
2026-08-08 16:53:31 +0530

    added event entity

--- booking-service/src/main/java/com/aman/bookingservice/entity/Event.java
+++ booking-service/src/main/java/com/aman/bookingservice/entity/Event.java
@@ -1,4 +1,43 @@
 package com.aman.bookingservice.entity;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "events")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Event {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String venue;
+
+	@Column(nullable = false)
+	private LocalDateTime eventTime;
+
+	@Column(nullable = false)
+	private boolean active = true;
+
+	@OneToMany(mappedBy = "event" , cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<TicketType> ticketTypes = new ArrayList<>();
 }
