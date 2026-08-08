9b9fbf5078fb4d28ae1864dd33724b2383db542e
Aman Gautam <amangautam2128@gmail.com>
2026-08-08 17:30:25 +0530

    added endpoint to create reservation

--- booking-service/src/main/java/com/aman/bookingservice/controller/ReservationController.java
+++ booking-service/src/main/java/com/aman/bookingservice/controller/ReservationController.java
@@ -1,4 +1,29 @@
 package com.aman.bookingservice.controller;
 
+import com.aman.bookingservice.dto.CreateReservationRequest;
+import com.aman.bookingservice.dto.ReservationResponse;
+import com.aman.bookingservice.service.ReservationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/reservations")
+@RequiredArgsConstructor
 public class ReservationController {
+
+	private final ReservationService reservationService;
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ReservationResponse createReservation(@Valid @RequestBody CreateReservationRequest request) {
+		return reservationService.createReservation(request);
+	}
+
 }
