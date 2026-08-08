9b7eac6db7788e0eda6a037fae7bc3d88077bc5f
Aman Gautam <amangautam2128@gmail.com>
2026-08-08 23:27:58 +0530

    Added redis inventory

--- booking-service/src/main/java/com/aman/bookingservice/config/RedisConfig.java
+++ booking-service/src/main/java/com/aman/bookingservice/config/RedisConfig.java
@@ -1,12 +1,19 @@
 package com.aman.bookingservice.config;
 
+import lombok.Builder;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 
 @Configuration
 public class RedisConfig {
 
 
+	@Bean
 	public DefaultRedisScript<Long> reserveInventoryScript() {
 		String script = """
                 local stock = redis.call('GET', KEYS[1])
@@ -28,8 +35,19 @@ public class RedisConfig {
 		redisScript.setScriptText(script);
 		redisScript.setResultType(Long.class);
 		return redisScript;
+	}
 
+	@Bean
+	LettuceConnectionFactory connectionFactory() {
+		return new LettuceConnectionFactory();
+	}
 
+	@Bean
+	RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+
+		RedisTemplate<String, String> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		return template;
 	}
 
 
--- booking-service/src/main/java/com/aman/bookingservice/service/RedisInventoryService.java
+++ booking-service/src/main/java/com/aman/bookingservice/service/RedisInventoryService.java
@@ -1,11 +1,49 @@
 package com.aman.bookingservice.service;
 
+import java.util.Collections;
+
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class RedisInventoryService {
 	private static final String INVENTORY_KEY_PREFIX = "ticket:inventory:";
+
+	private final StringRedisTemplate redisTemplate = new StringRedisTemplate();
+	private final DefaultRedisScript<Long> reserveInventoryScript;
+
+	public void initializeInventory(Long ticketTypeId, int quantity){
+		String key = getInventoryKey(ticketTypeId);
+		redisTemplate.opsForValue().set(key, String.valueOf(quantity));
+	}
+
+	private String getInventoryKey(Long ticketTypeId) {
+		return INVENTORY_KEY_PREFIX + ticketTypeId;
+	}
+
+	private long getInventory(Long ticketTypeId) {
+		String key = getInventoryKey(ticketTypeId);
+		String value = redisTemplate.opsForValue().get(key);
+		if (value == null) {return -1;}
+		return Long.parseLong(value);
+	}
+
+	public boolean reserveInventory(Long ticketTypeId, int quantity){
+		String key = getInventoryKey(ticketTypeId);
+		Long result = redisTemplate.execute(reserveInventoryScript,
+				Collections.singletonList(key),
+				String.valueOf(quantity)
+		);
+		return result != null && result == 1;
+	}
+
+	public void releaseInventory(Long ticketTypeId, int quantity) {
+		String key = getInventoryKey(ticketTypeId);
+		redisTemplate.opsForValue().increment(key, quantity);
+	}
+
 }
