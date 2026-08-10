1ac301233eb53d495da580c0da0eb1d924828a91
Aman Gautam <amangautam2128@gmail.com>
2026-08-10 23:54:57 +0530

    updated RedisInventoryService

--- inventory-service/src/main/java/com/aman/inventoryservice/service/RedisInventoryService.java
+++ inventory-service/src/main/java/com/aman/inventoryservice/service/RedisInventoryService.java
@@ -14,32 +14,21 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class RedisInventoryService {
-
 	private final StringRedisTemplate redisTemplate;
-	//private final DefaultRedisScript<Long> releaseInventoryScript();
+	private final DefaultRedisScript<Long> releaseInventoryScript;
 
 	public boolean releaseInventory(Long ticketTypeId, int quantity, String eventId) {
 
 		String inventoryKey = "inventory:" + ticketTypeId;
 		String processedKey = "inventory:release:processed:" + eventId;
-
-		Long result = redisTemplate.execute(releaseInventoryScript(), List.of(inventoryKey, processedKey),
+		Long result = redisTemplate.execute(
+						releaseInventoryScript,
+						List.of(
+								inventoryKey,
+								processedKey
+						),
 						String.valueOf(quantity)
 				);
-
 		return result != null && result == 1;
 	}
-
-	private DefaultRedisScript<Long> releaseInventoryScript() {
-		DefaultRedisScript<Long> script = new DefaultRedisScript<>();
-		script.setLocation(
-				new ClassPathResource(
-						"scripts/release_inventory.lua"
-				)
-		);
-		script.setResultType(Long.class);
-		return script;
-	}
-
-
 }
